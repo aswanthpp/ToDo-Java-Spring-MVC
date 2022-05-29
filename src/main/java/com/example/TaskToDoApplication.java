f17afb50395d6cde866ed07145448b8a0924bef3
Z0044J2 <aswanth.p@target.com>
2022-05-29 23:06:58 +0530

    spring connection string updated

--- src/main/java/com/example/TaskToDoApplication.java
+++ src/main/java/com/example/TaskToDoApplication.java
@@ -11,12 +11,6 @@ public class TaskToDoApplication {
 		ApplicationContext ctx =SpringApplication.run(TaskToDoApplication.class, args);
 		System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-
-//		String[] beanNames = ctx.getBeanDefinitionNames();
-//		Arrays.sort(beanNames);
-//		for (String beanName : beanNames) {
-//			System.out.println(beanName);
-//		}
 	}
 
 }
