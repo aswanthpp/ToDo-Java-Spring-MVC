dba2e4e305921420aa3dc78632209960235127fc
Z0044J2 <aswanth.p@target.com>
2022-05-29 23:13:51 +0530

    spring connection string updated

--- src/main/java/com/example/dataaccess/mysql/TestMySQLConnection.java
+++ src/main/java/com/example/dataaccess/mysql/TestMySQLConnection.java
@@ -3,12 +3,8 @@ package com.example.dataaccess.mysql;
 import java.sql.Connection;
 
 public class TestMySQLConnection {
-    public void testConnection() {
-        try {
-            DatabaseManager.getConnection();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+        DatabaseManager.getConnection();
     }
 }
 
